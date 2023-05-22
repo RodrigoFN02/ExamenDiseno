5e2fd3125df8ceb12f557a34dbc211820d3edc03
Rodrigo <rodrigo.fernun@educa.jcyl.es>
2023-05-22 20:53:43 +0200

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './vistas/componentes/navbar/navbar.component';
+import { CardComponent } from './vistas/componentes/card/card.component';
+import { FooterComponent } from './vistas/componentes/footer/footer.component';
+import { FotoslondresComponent } from './vistas/fotoslondres/fotoslondres.component';
+import { ParallaxComponent } from './vistas/parallax/parallax.component';
+
+//Material
+
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CardComponent,
+    FooterComponent,
+    FotoslondresComponent,
+    ParallaxComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
